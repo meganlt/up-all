8678f7aae8023d40808230951203acfa78a0c767
Megan Tegeder <megan.tegeder@gmail.com>
2025-04-03 18:52:07 -0500

    adding paper to admin manage users

--- src/components/AdminManageUsers/AdminManagerUsers.jsx
+++ src/components/AdminManageUsers/AdminManagerUsers.jsx
@@ -1,6 +1,28 @@
 import { useState, useEffect } from 'react';
 import useStore from '../../zustand/store';
 import AdminEditUser from '../AdminEditUser/AdminEditUser';
+import {
+  Container,
+  Paper,
+  Typography,
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Tabs,
+  Tab,
+  AppBar,
+  Toolbar,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormLabel,
+  RadioGroup,
+  Radio,
+  FormControl,
+  Grid2,
+  TextField
+} from '@mui/material';
 
 function AdminManageUsers() {
   const pendingUsers = useStore((state) => state.pendingUsers);
@@ -16,60 +38,65 @@ function AdminManageUsers() {
   return (
     <>
       <h1>Manage Users</h1>
+      <Paper elevation={1} sx={{ p: 4, mb: 4 }}>
       <div>
-        <h2>Users To Assign:</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Email</th>
-              <th>Username</th>
-              <th>Company</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-          {
-            pendingUsers.map( (userToEdit, index)=>(
-              <tr key={index}>
-                <td>{userToEdit.email}</td>
-                <td>{userToEdit.username}</td>
-                <td>{userToEdit.company}</td>
-                <td><AdminEditUser userToEdit={userToEdit}/></td>
+          <h2>Users To Assign:</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Email</th>
+                <th>Username</th>
+                <th>Company</th>
+                <th>Actions</th>
               </tr>
-            ))
-          }
-          </tbody>
-        </table>
-      </div>
-      <div>
-        <h2>All Users:</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Email</th>
-              <th>Username</th>
-              <th>Role</th>
-              <th>Manager Assigned</th>
-              <th>Company</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-          {
-            assignedUsers.map( (userToEdit, index)=>(
-              <tr key={index}>
-                <td>{userToEdit.email}</td>
-                <td>{userToEdit.username}</td>
-                <td>{userToEdit.role}</td>
-                <td>{userToEdit.manager_username}</td>
-                <td>{userToEdit.company}</td>
-                <td><AdminEditUser userToEdit={userToEdit}/></td>
+            </thead>
+            <tbody>
+            {
+              pendingUsers.map( (userToEdit, index)=>(
+                <tr key={index}>
+                  <td>{userToEdit.email}</td>
+                  <td>{userToEdit.username}</td>
+                  <td>{userToEdit.company}</td>
+                  <td><AdminEditUser userToEdit={userToEdit}/></td>
+                </tr>
+              ))
+            }
+            </tbody>
+          </table>
+        </div>
+      </Paper>
+      <Paper elevation={1} sx={{ p: 4, mb: 4 }}>
+        <div>
+          <h2>All Users:</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Email</th>
+                <th>Username</th>
+                <th>Role</th>
+                <th>Manager Assigned</th>
+                <th>Company</th>
+                <th>Actions</th>
               </tr>
-            ))
-          }
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+            {
+              assignedUsers.map( (userToEdit, index)=>(
+                <tr key={index}>
+                  <td>{userToEdit.email}</td>
+                  <td>{userToEdit.username}</td>
+                  <td>{userToEdit.role}</td>
+                  <td>{userToEdit.manager_username}</td>
+                  <td>{userToEdit.company}</td>
+                  <td><AdminEditUser userToEdit={userToEdit}/></td>
+                </tr>
+              ))
+            }
+            </tbody>
+          </table>
+        </div>
+      </Paper>
+      
     </>
   );
 }
